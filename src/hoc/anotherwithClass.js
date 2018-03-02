dbfeb72202491be4feb537de0efdad422ec8ca34
Susheel <susheelbajaj@github.com>
2018-03-02 14:44:33 -0500

    different example of using HOC

--- src/hoc/anotherwithClass.js
+++ src/hoc/anotherwithClass.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// const withClass = (WrappedComponent, className) => {
+//     return (props) =>(
+//         <div className={className}>
+//             <WrappedComponent {...props}/>
+//         </div>
+//     )
+// }
 
 const withClass = (WrappedComponent, className) => {
-    return (props) =>(
-        <div className={className}>
-            <WrappedComponent {...props}/>
-        </div>
-    )
+    return class extends Component { //no class name required as it is anonymous class
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props}/>
+                </div>
+            )
+        }
+    }
 }
 
 export default withClass;
